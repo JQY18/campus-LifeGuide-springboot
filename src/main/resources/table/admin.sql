574ed8b0b6424cab59ac14e425157a2447e05e66
JQY18 <3165973561@qq.com>
2025-01-13 11:13:13 +0800

    增加数据库建立以及数据导入的脚本

--- src/main/resources/table/admin.sql
+++
@@ -1,2 +0,0 @@
-insert into campus_trade.admin (id, username, password)
-values  (1, 'admin', '111111');
--- src/main/resources/table/建表SQL语句.sql
+++ src/main/resources/table/建表SQL语句.sql
@@ -1,14 +1,22 @@
-create database campus_trade;
-
+create database if not exists campus_trade;
 use campus_trade;
+
+drop table if exists admin;
 
 create table admin
 (
-    id       int auto_increment
+    id           int auto_increment
         primary key,
-    username varchar(20) null,
-    password varchar(20) null
+    username     varchar(20)                        null,
+    password     varchar(20)                        null,
+    location_id  int                                null comment '该管理员能够管理的地点，0表示为超级管理员',
+    created_time datetime default CURRENT_TIMESTAMP null,
+    constraint admin_pk
+        unique (username)
 );
+
+
+drop table if exists comment;
 
 create table comment
 (
@@ -22,6 +30,9 @@ create table comment
     update_time  datetime default (now()) null
 );
 
+drop table if exists detail_comment;
+
+
 create table detail_comment
 (
     id          int auto_increment
@@ -32,6 +43,9 @@ create table detail_comment
     create_time datetime default (now()) null
 );
 
+
+drop table if exists images;
+
 create table images
 (
     id          int auto_increment
@@ -40,6 +54,9 @@ create table images
     url         varchar(255)             null,
     create_time datetime default (now()) null
 );
+
+drop table if exists likes;
+
 
 create table likes
 (
@@ -50,18 +67,25 @@ create table likes
     create_time datetime default (now()) null
 );
 
+drop table if exists location;
+
+
 create table location
 (
-    id          bigint auto_increment
+    id           bigint auto_increment
         primary key,
-    name        varchar(255) not null,
-    description text         null,
-    image       varchar(255) null,
-    detail_id   varchar(255) null,
-    category    varchar(255) null,
-    x           double       null,
-    y           double       null
+    name         varchar(255)                       not null,
+    description  text                               null,
+    image        varchar(255)                       null,
+    detail_id    varchar(255)                       null,
+    category     varchar(255)                       null,
+    x            double                             null,
+    y            double                             null,
+    created_time datetime default CURRENT_TIMESTAMP null
 );
+
+drop table if exists location_detail;
+
 
 create table location_detail
 (
@@ -70,26 +94,34 @@ create table location_detail
     detail_id   varchar(255)             null,
     name        varchar(255)             not null,
     description text                     null,
-    image_id    int                      null,
-    video_id    int                      null,
     create_time datetime default (now()) null
 );
+
+drop table if exists location_images;
+
 
 create table location_images
 (
-    id          int auto_increment
+    id                 int auto_increment
         primary key,
-    url         varchar(255)             null,
-    create_time datetime default (now()) null
+    url                varchar(255)             null,
+    create_time        datetime default (now()) null,
+    location_detail_id int                      null comment 'location_detail_id'
 );
+
+drop table if exists location_videos;
+
 
 create table location_videos
 (
-    id          int auto_increment
+    id                 int auto_increment
         primary key,
-    url         varchar(255)             null,
-    create_time datetime default (now()) null
+    url                varchar(255)             null,
+    create_time        datetime default (now()) null,
+    location_detail_id int                      null
 );
+
+drop table if exists post;
 
 create table post
 (
@@ -103,6 +135,19 @@ create table post
     update_time datetime default (now()) null
 );
 
+
+drop table if exists post_likes;
+
+create table post_likes
+(
+    id      int auto_increment
+        primary key,
+    post_id int null,
+    user_id int null
+);
+
+drop table if exists reply;
+
 create table reply
 (
     id           int auto_increment
@@ -113,6 +158,18 @@ create table reply
     content      varchar(255)             not null,
     create_time  datetime default (now()) null
 );
+
+drop table if exists statistics;
+
+create table statistics
+(
+    date  date             not null comment '日期',
+    count bigint default 0 null comment '当日访问量'
+)
+    comment '统计访问量';
+
+
+drop table if exists user;
 
 create table user
 (
