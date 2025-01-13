5cdcdb13799085465a582893b4c1cf1347c72dd3
JQY18 <3165973561@qq.com>
2025-01-13 09:45:40 +0800

    完成

---
+++ src/main/java/renko/jiang/campus_trade/controller/admin/pojo/dto/AdminDTO.java
@@ -0,0 +1,11 @@
+package renko.jiang.campus_trade.controller.admin.pojo.dto;
+
+import lombok.Data;
+
+@Data
+public class AdminDTO {
+    private Integer id;
+    private String username;
+    private String password;
+    private Integer locationId;
+}
---
+++ src/main/java/renko/jiang/campus_trade/mapper/StatisticsMapper.java
@@ -0,0 +1,32 @@
+package renko.jiang.campus_trade.mapper;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.time.LocalDate;
+
+/**
+ * @author 86132
+ */
+
+@Mapper
+public interface StatisticsMapper {
+
+    @Select("select count from statistics where date = #{date}")
+    Long getCount(LocalDate date);
+
+    @Insert("insert into statistics (date,count) value (#{date},1)")
+    Integer addVisit(LocalDate date);
+
+    @Update("update statistics set count = count + 1 where date = #{date}")
+    Integer update(LocalDate date);
+
+    @Select("select sum(count) from statistics where date >= #{from}")
+    Long getWeekCount(LocalDate from);
+
+    @Select("select sum(count) from statistics")
+    Long getAllCount();
+
+}
